f22d34286367e3e77d2c451a7128087548b3ca72
kennethghell <kenetghellb@gmail.com>
2022-02-12 04:16:20 +0800

    -initial commit 2

---
+++ DownloadAndView/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DownloadandviewConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'DownloadAndView'
--- FileRepository/wsgi.py
+++ FileRepository/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for FileRepositoryWebApp project.
+WSGI config for FileRepository project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FileRepositoryWebApp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FileRepository.settings')
 
 application = get_wsgi_application()
