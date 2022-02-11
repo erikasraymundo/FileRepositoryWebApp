df07f43fbad221564fe8f3581939ac863dbda33c
kennethghell <kenetghellb@gmail.com>
2022-02-12 05:03:28 +0800

    reset

--- AccountRegistration/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AccountregistrationConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'AccountRegistration'
--- LoginAndLogout/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class LoginandlogoutConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'LoginAndLogout'
