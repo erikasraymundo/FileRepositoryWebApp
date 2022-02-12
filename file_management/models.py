3a5867e842a6c47785c3c3d6a413515e46e27671
erikasraymundo <erika.raymundo.s@bulsu.edu.ph>
2022-02-12 14:52:14 +0800

    table for file management

--- file_management/models.py
+++ file_management/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from category_management.models import Category
+from users_management.models import CustomUser
 
 # Create your models here.
 
@@ -8,3 +10,5 @@ class File(models.Model):
     description = models.CharField(max_length = 200)
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField()
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+    customuser_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
