a10634e29b75b89e57df08098d963242245f644e
erikasraymundo <erika.raymundo.s@bulsu.edu.ph>
2022-02-13 00:03:23 +0800

    delete sa main

--- users_management/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-
-def profile(request):
-    return render(request, 'profile.html')
-
-def manage_accounts(request):
-    return render(request, 'manage-accounts.html')
