ad0f1803b27f4065e5bf45aaf2725f77c36115d4
kennethghell <kenetghellb@gmail.com>
2022-02-12 23:48:24 +0800

    reset

--- activity_log/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ActivityLogConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'activity_log'
--- category_management/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Category(models.Model):
-    title = models.CharField(max_length=50)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
