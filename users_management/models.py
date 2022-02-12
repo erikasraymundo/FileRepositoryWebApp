cd5771a572f1c7de178ad7477a1938594bae45cb
erikasraymundo <erika.raymundo.s@bulsu.edu.ph>
2022-02-12 16:11:24 +0800

    Update models.py

--- users_management/models.py
+++ users_management/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
     
 
-class User(AbstractUser):
+class User(AbstractUser): HAHAHAHAHHAHAH
     user_type = models.PositiveSmallIntegerField()
     middle_name = models.CharField(max_length=200, null=True)
     gender = models.PositiveSmallIntegerField()
