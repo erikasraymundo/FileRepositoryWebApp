5b33d54d627b8228ef1fb10405ee87df596684f8
kenetghell21 <60879598+kenetghell21@users.noreply.github.com>
2022-02-14 05:30:23 +0800

    -done category management

--- category_management/views.py
+++ category_management/views.py
@@ -1,16 +1,16 @@
 from re import template
 from django.shortcuts import render
-from .models import Category
-from django.http import HttpResponse
 
+from users_management.models import User
+from .models import Category
+from activity_log.models import Log
 
 def categoryManagement(request):
-    categoryNames = Category.objects.all()
     return render(request, 'category-management.html', {
-        'categories' : categoryNames,
+        'categories' : Category.objects.filter(isArchived = False),
     })
 
-def SaveACategory(request):
+def AddCategory(request):
     if(request.POST['CategoryInput'] == ''):
         categoryNames = Category.objects.all()
         return render(request, 'category-management.html', {  'categories' : categoryNames, })
@@ -18,11 +18,48 @@ def SaveACategory(request):
         categoryNames = Category.objects.all()
         for category in categoryNames:
             if(category.title ==  request.POST['CategoryInput']):
+                if(category.isArchived == True):
+                    category.isArchived = False
+                    category.save()
+                    log = Log()
+                    log.user_id = User.objects.get(pk=1)
+                    log.description = category.title + ' has been added to active categories from the archive'
+                    log.save()
                 break
+
         else:
             category = Category()
             category.title = request.POST['CategoryInput']
             category.save()
-    
-        categoryNames = Category.objects.all()
-        return render(request, 'category-management.html', {  'categories' : categoryNames, })
+            log = Log()
+            log.user_id = User.objects.get(pk=1)
+            log.description = category.title + ' has been added to categories'
+            log.save()
+    return render(request, 'category-management.html', {
+        'categories' : Category.objects.filter(isArchived = False),
+    })
+
+def DeleteCategory(request):
+    entry = Category.objects.get(pk = request.POST['ID'])
+    entry.isArchived =True
+    entry.save()
+    log = Log()
+    log.user_id = User.objects.get(pk=1)
+    log.description = entry.title + ' has been moved to the archived categories'
+    log.save()
+    return render(request, 'category-management.html', {
+        'categories' : Category.objects.filter(isArchived = False),
+    })
+
+def UpdateCategory(request):
+    entry = Category.objects.get(pk = request.POST['categoryID'])
+    entry.title = request.POST['newCategoryName']
+    entry.save()
+
+    log = Log()
+    log.user_id = User.objects.get(pk=1)
+    log.description = 'Category named ' + Category.objects.get(pk = request.POST['categoryID']).title + ' was renamed to ' + request.POST['newCategoryName']
+    log.save()
+    return render(request, 'category-management.html', {
+        'categories' : Category.objects.filter(isArchived = False),
+    })
