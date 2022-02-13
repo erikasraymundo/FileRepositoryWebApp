84486d3929e1fbfa615f5d7ac4a8268afc15595f
erikasraymundo <erika.raymundo.s@bulsu.edu.ph>
2022-02-13 13:26:43 +0800

    Search done on file management

--- file_management/urls.py
+++ file_management/urls.py
@@ -5,8 +5,10 @@ app_name = "file-management"
 urlpatterns = [
     path('category=<int:category_id>/', views.index, name='category'),
     path('sort=<int:sort_by>/', views.index, name='sortBy'),
-    path('category=<int:category_id>/sort=<int:sort_by>/',
+    path('category=<int:category_id>/sort=<int:sort_by>',
          views.index, name='filter'),
+    path('category=<int:category_id>/sort=<int:sort_by>/search=<str:query>',
+         views.index, name='search'),
     path('', views.index, name='index'),
     path('int<pk>', views.DetailView.as_view(), name='detail'),
     path('upload', views.upload, name='upload'),
--- file_management/views.py
+++ file_management/views.py
@@ -1,14 +1,17 @@
 from unicodedata import category
 from django.shortcuts import render
 from django.views import generic
+from django.db.models import Q
+
 
 from category_management.models import Category
 from . models import File
 
 
-def index(request,  category_id=0, sort_by=1):
+def index(request,  category_id=0, sort_by=1, query=None):
     template_name = 'file_management/index.html'
 
+    if query == None: query = ""
     sort_column = "name"
 
     if (sort_by == 2):
@@ -19,14 +22,24 @@ def index(request,  category_id=0, sort_by=1):
         sort_column = "created_at"
 
     if (category_id > 0):
-        file_list = File.objects.filter(category_id=category_id).order_by(sort_column)
+            file_list = File.objects.filter(
+                Q(name__icontains=query) | 
+                Q(category_id__title__icontains=query) |
+                Q(user_id__first_name__icontains=query),
+                category_id=category_id
+            ).order_by(sort_column)
     else:
-        file_list = File.objects.filter().order_by(sort_column)
+        file_list = File.objects.filter(
+            Q(name__icontains=query) |
+            Q(category_id__title__icontains=query) |
+            Q(user_id__first_name__icontains=query),
+            ).order_by(sort_column)
 
     return render(request, template_name, 
     {"file_list": file_list,
      "category_selected" : category_id,
      "sort_selected": sort_by,
+     "search_value": query,
      "category_list": Category.objects.all()})
 
 class DetailView(generic.DetailView):
