3203a8bba426c8f782c0894db6ce8ef1ab06be21
kenetghell21 <60879598+kenetghell21@users.noreply.github.com>
2022-02-14 05:31:38 +0800

    added my urls

--- file_repository_web_app/urls.py
+++ file_repository_web_app/urls.py
@@ -19,7 +19,7 @@ from accounts.views import login
 from accounts.views import register
 from users_management.views import profile
 from users_management.views import manage_accounts, add_accounts, edit_accounts, archive_accounts, view_accounts
-from category_management.views import categoryManagement, SaveACategory
+from category_management.views import categoryManagement, AddCategory, UpdateCategory, DeleteCategory
 from activity_log.views import view_logs
 from django.urls import include, path
 
@@ -34,7 +34,9 @@ urlpatterns = [
     path('profile/', profile, name='profile'),
     path('manage-accounts/', manage_accounts, name='manage_accounts'),
     path('category/', categoryManagement, name='categoryManagement'),
-    path('category', SaveACategory, name='SaveACategory'),
+    path('category/add', AddCategory, name='AddCategory'),
+    path('category/update', UpdateCategory, name='UpdateCategory'),
+    path('category/delete', DeleteCategory, name='DeleteCategory'),
     path('add-accounts/', add_accounts, name='add_accounts'),
     path('edit-accounts/', edit_accounts, name='edit_accounts'),
     path('archive-accounts/', archive_accounts, name='archive_accounts'),
