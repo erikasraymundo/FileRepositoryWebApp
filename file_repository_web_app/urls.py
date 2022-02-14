e3c7d903f02c27b8073b91aa46c61d1627e6c15f
kenetghell21 <60879598+kenetghell21@users.noreply.github.com>
2022-02-14 08:38:11 +0800

    added modal on category delete, complete MyProfile part

--- file_repository_web_app/urls.py
+++ file_repository_web_app/urls.py
@@ -17,8 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from accounts.views import login
 from accounts.views import register
-from users_management.views import profile
-from users_management.views import manage_accounts, add_accounts, edit_accounts, archive_accounts, view_accounts
+from users_management.views import profile, UpdatePassword, DeleteAccount, UpdateAccountDetails
 from category_management.views import categoryManagement, AddCategory, UpdateCategory, DeleteCategory
 from activity_log.views import view_logs
 from django.urls import include, path
@@ -32,15 +31,13 @@ urlpatterns = [
     path('login/', login, name='login'),
     path('registration/', register, name='register'),
     path('profile/', profile, name='profile'),
-    path('manage-accounts/', manage_accounts, name='manage_accounts'),
+    path('profile/update-password', UpdatePassword, name='UpdatePassword'),
+    path('profile/delete-account', DeleteAccount, name='DeleteAccount'),
+    path('profile/update-account', UpdateAccountDetails, name='UpdateAccountDetails'),
     path('category/', categoryManagement, name='categoryManagement'),
     path('category/add', AddCategory, name='AddCategory'),
     path('category/update', UpdateCategory, name='UpdateCategory'),
     path('category/delete', DeleteCategory, name='DeleteCategory'),
-    path('add-accounts/', add_accounts, name='add_accounts'),
-    path('edit-accounts/', edit_accounts, name='edit_accounts'),
-    path('archive-accounts/', archive_accounts, name='archive_accounts'),
-    path('view-accounts/', view_accounts, name='view_accounts'),
     path('activity-logs/', view_logs, name='view_logs'),
 ]
 urlpatterns += static(settings.MEDIA_URL,
--- users_management/views.py
+++ users_management/views.py
@@ -1,21 +1,38 @@
 from django.shortcuts import render
-
+from .models import User
+from django.utils import timezone
 
 def profile(request):
-    return render(request, 'profile/profile.html')
+    return render(request, 'profile/profile.html', {
+        'details' : User.objects.filter(pk =1, deleted_at__isnull = True),
+    })
 
-def manage_accounts(request):
-    return render(request, 'user-accounts/manage-accounts.html')
+def UpdatePassword(request):
+    admin = User.objects.get(pk = 1)
+    admin.password = request.POST['newPassword']
+    admin.save()
+    return render(request, 'profile/profile.html', {
+        'details' : User.objects.filter(pk =1, deleted_at__isnull = True),
+    })
 
-def add_accounts(request):
-    return render(request, 'user-accounts/add-account.html')
+def DeleteAccount(request):
+    admin = User.objects.get(pk = request.POST['DeleteID'])
+    admin.deleted_at = timezone.now()
+    admin.updated_at = timezone.now()
+    admin.save()
+    return render(request, 'profile/profile.html', {
+        'details' : User.objects.filter(pk =1, deleted_at__isnull = True),
+    })
 
-def edit_accounts(request):
-    return render(request, 'user-accounts/edit-account.html')
-
-def archive_accounts(request):
-    return render(request, 'user-accounts/archive-account.html')
-
-def view_accounts(request):
-    return render(request, 'user-accounts/view-account.html')
-
+def UpdateAccountDetails(request):
+    admin = User.objects.get(pk = request.POST['DeleteID'])
+    admin.updated_at = timezone.now()
+    admin.username = request.POST['username']
+    admin.first_name = request.POST['first_name']
+    admin.last_name = request.POST['last_name']
+    admin.address = request.POST['address']
+    admin.email = request.POST['email']
+    admin.save()
+    return render(request, 'profile/profile.html', {
+        'details' : User.objects.filter(pk =1, deleted_at__isnull = True),
+    })
