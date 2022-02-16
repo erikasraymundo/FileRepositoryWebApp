801d953191b649eb34729f739bdf5819b01e6b0c
erikasraymundo <erika.raymundo.s@bulsu.edu.ph>
2022-02-16 13:18:11 +0800

    login backend done

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,9 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.loginView, name='index'),
-    path('login', views.loginView, name='loginView'),
+    path('login/', views.loginView, name='loginView'),
+    path('login/<int:error>', views.loginView, name='loginViewInvalid'),
     path('login/verification', views.login, name='login'),
     path('registration', views.registerView, name='registerView'),
     path('registration/verification', views.register, name='register'),
-    path('logout', views.logout, name='registerView')
+    path('logout', views.logout, name='logout')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -3,21 +3,54 @@ from django.shortcuts import render
 
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from users_management.models import User
 
-def loginView(request):
-    return render(request, 'accounts/login.html')
+def loginView(request, error = 0):
+    return render(request, 'accounts/login.html', {"error": error})
 
 # need po itong baguhin
 def login(request):
-    setupSession(request, 2, False)
+
+    usernameEmail = request.POST['usernameEmail']
+    password = request.POST['password']
+
+    userUsername = User.objects.filter(
+        username__iexact=usernameEmail, password=password).first()
+    userEmail = User.objects.filter(
+        email__iexact=usernameEmail, password=password).first()
+
+    if userUsername != None:
+        setupSession(request, userUsername.id, userUsername.is_superuser)
+    elif userEmail != None:
+        setupSession(request, userEmail.id, userEmail.is_superuser)
+    else:
+        return HttpResponseRedirect(reverse('loginViewInvalid', args=[1]))
+
     return HttpResponseRedirect(reverse('file-management:index'))
 
 
-def registerView(request):
-    return render(request, 'accounts/registration.html')
+def registerView(request, error=0):
+    return render(request, 'accounts/registration.html', {"error": error})
 
 # need po itong baguhin
 def register(request):
+
+    user = User()
+    user.username = request.POST['username']
+    user.first_name = request.POST['first_name']
+    user.last_name = request.POST['last_name']
+    user.middle_name = request.POST['middle_name']
+    user.address = request.POST['address']
+    user.email = request.POST['email']
+    user.gender = request.POST['group']
+    birthdate = request.POST['birthday']
+    user.birthdate = datetime.datetime(int(birthdate[6:10]), int(birthdate[0:2]), int(birthdate[3:5]))
+    user.date_joined =  timezone.now()
+    user.is_superuser = False
+    user.is_staff = False
+    user.user_type = 1
+    user.save()
+
     setupSession(request, 2, False)
     return HttpResponseRedirect(reverse('login'))
 
