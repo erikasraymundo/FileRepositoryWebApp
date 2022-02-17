14f5a6e766f4ce7a63906daa19e7123e0230c0c2
erikasraymundo <erika.raymundo.s@bulsu.edu.ph>
2022-02-18 01:09:53 +0800

    Mass replace animations

--- file_repository_web_app/urls.py
+++ file_repository_web_app/urls.py
@@ -1,6 +1,6 @@
 """file_repository_web_app URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
+The `urlpatterns` list routes URLs to views. For more information pllinear see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
 Examples:
 Function views
